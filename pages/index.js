59b074c57dda4c19a0aaa82eda827de217292807
Rafael Lopes <rafaellopes.dev@gmail.com>
2023-08-02 04:28:31 +0000

    other: Informe de página em construção.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Página em Construção</h1>
+      <p>Esta página faz parte do projeto open source de criação do clone da tabnews.com.br</p>
+    </>
+  );
 }
 
 export default Home;
